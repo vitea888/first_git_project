987357bdde2a8479ba4c983e6e51ccfdf9626fb7
vitea888 <vitea88@gmail.com>
2025-09-09 10:03:15 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Helo World");
+        System.out.println("Helo CYDEO");
+        System.out.println("Helo Programmers");
+        
     }
 }
